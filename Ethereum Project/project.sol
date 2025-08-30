ecd90f0cee8647adbf6bf9397c28d506b9852824
cronoscar <eucedao662@gmail.com>
2025-08-30 11:03:41 -0600

    prueba

--- Ethereum Project/project.sol
+++ Ethereum Project/project.sol
@@ -1,5 +1,26 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract MyContract {
-    // Contract code goes here
+    enum Role {None, Auditor, Donante, Organizacion}
+
+    struct User{
+        address wallet;
+        Role role;
+    }
+
+    mapping(address => User) public users;
+
+    function registerUser(Role _role) public {
+        require(users[msg.sender].wallet == address(0), "Ya registrado");
+        users[msg.sender] = User(msg.sender, _role);
+    }
+
+    function login() public view returns (Role) {
+        require(users[msg.sender].role != Role.None, "No registrado");
+        return users[msg.sender].role;
+    }
+
+    
+
 }   
+
